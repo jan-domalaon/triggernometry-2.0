08214ea539bdbb5befa2a251ef5d3b18254483a8
jandomalaon@gmail.com <jandomalaon@gmail.com>
2024-09-26 22:10:50 -0400

    Initial creation of environmental items. Work on player script. Added BG in space lvl

--- player/player.gd
+++ player/player.gd
@@ -3,8 +3,17 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const GRAVITY = 800
+
+var direction : float
 
 @export var player_number : int = 1
+
+func _ready() -> void:
+	if player_number == 1:
+		$Sprite2D.set_texture('red_player.png')
+	elif player_number == 2:
+		$Sprite2D.set_texture('blue_player.png')
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -12,12 +21,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed('p1_jump') and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis('p1_left', 'p1_right')
 	if direction:
 		velocity.x = direction * SPEED
 	else:
